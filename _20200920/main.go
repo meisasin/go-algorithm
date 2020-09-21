5d6cb67c44d4d6ee6efa99f05bfb394d41ce04d3
maxiaofeng <yisasin_clover@outlook.com>
2020-09-21 10:08:13 +0800

    :art:  78. 子集        给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。        说明：解集不能包含重复的子集。

--- _20200920/main.go
+++ _20200920/main.go
@@ -4,6 +4,20 @@ func main() {
 
 }
 
+func subsets_bit(nums []int) (ans [][]int) {
+	n := len(nums)
+	for mask := 0; mask < 1<<n; mask++ {
+		set := []int{}
+		for i, v := range nums {
+			if mask>>i&1 > 0 {
+				set = append(set, v)
+			}
+		}
+		ans = append(ans, append([]int(nil), set...))
+	}
+	return
+}
+
 func subsets(nums []int) (ans [][]int) {
 	set := []int{}
 	var dfs func(int)
--- _20200920/me_test.go
+++ _20200920/me_test.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestSubsets(t *testing.T) {
 
+}
+
+func TestSubsets_bit(t *testing.T) {
+
+	fmt.Println(subsets_bit([]int{1, 2, 3}))
+}
+
+func TestMask(t *testing.T) {
+
+	//i := 7
+	//for mask := 0 ; mask < 7 ; mask ++ {
+	//	if mask >> i & 1 > 0 {
+	//
+	//	}
+	//}
+
+	//110
+	mask := 6
+	mover := 0
+	fmt.Println(mask >> mover)
+	fmt.Println(mask >> mover & 1)
+
+	fmt.Println(7 & 1)
 }
